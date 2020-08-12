e365fcf0ad4ec039d19a01da04d8a928918b0249
Daniel Escobar <dany.dim@hotmail.com>
2020-08-12 16:54:40 -0500

    This function normalize the values of a Matrix
    This function normalize the values of a Matrix and also returns 2 vectors, one with mean values of columns and another one with std values of columns.

--- featureNormalize.m
+++ featureNormalize.m
@@ -17,6 +17,6 @@ function [X_norm, mu, sigma] = featureNormalize(X) % FEATURENORMALIZE Normalizes
 		for j=1:size(X,1),
 		X_norm(j,i)=(X(j,i)-mu(1,i))/sigma(1,i); %This will set up the normalized values in the new matrix
 		end;
-	end;		
+	end;		 
 
 end
