8d8f6eb1cc30847f3e24c82f820383a3476f8135
Daniel Escobar <dany.dim@hotmail.com>
2020-08-12 02:57:28 -0500

    Delete warmUpExercise.m

--- warmUpExercise.m
+++
@@ -1,4 +0,0 @@
-function A = warmUpExercise() 	% Function that returns the 5x5 identity matrix
-  A = []						%Empatize
-  A = eye(5)                	%F
-end                           % End of the function
