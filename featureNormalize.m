18b4433fa6fd3b193ba3555f02360b256acfb2f9
Daniel Escobar <dany.dim@hotmail.com>
2020-08-12 16:50:38 -0500

    This function normalize the values of a matrix
    This function normalize the values of a matrix and also returns the mean and std of each column

---
+++ featureNormalize.m
@@ -0,0 +1,21 @@
+function [X_norm, mu, sigma] = featureNormalize(X) % FEATURENORMALIZE Normalizes the features in X 
+	% 	Returns a normalized version of X where the mean value of each feature is 0 and the standard deviationis 1. 
+	%	This is often a good preprocessing step to do when working with learning algorithms.
+
+	% size(X, 2) returns the number of features or columns in X matrix
+	X_norm = X;
+	mu = zeros(1, size(X, 2));
+	sigma = zeros(1, size(X, 2));
+   
+
+	mu= mean(X);
+	sigma=std(X);
+
+	% Use i for columns and j for rows
+	for i=1:size(X,2),
+		for j=1:size(X,1),
+		X_norm(j,i)=(X(j,i)-mu(1,i))/sigma(1,i); %This will set up the normalized values in the new matrix
+		end;
+	end;		
+
+end
