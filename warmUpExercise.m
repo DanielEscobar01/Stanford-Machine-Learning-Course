71ef4d7142753e8e36205d16fc18572caa6e21e9
Daniel Escobar <dany.dim@hotmail.com>
2020-08-12 02:52:18 -0500

    Update warmUpExercise.m

--- warmUpExercise.m
+++ warmUpExercise.m
@@ -1,4 +1,4 @@
 function A = warmUpExercise() % Function that returns the 5x5 identity matrix
-A =[];
-A = eye(5);                   
+A =[];                        % Initialize the matrix
+A = eye(5);                   % Creates identity matrix 5x5 that is going to be returned
 end                           % End of the function
