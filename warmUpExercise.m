32bf4af97b775e3f1c4812726a0bc062f514ecad
Daniel Escobar <dany.dim@hotmail.com>
2020-08-12 03:00:40 -0500

    Add files via upload

--- warmUpExercise.m
+++ warmUpExercise.m
@@ -1,4 +1,3 @@
 function A = warmUpExercise() 	% Function that returns the 5x5 identity matrix
-	A = []						% Nada
 	A = eye(5)                	% Nada
 end                           	% End of the function
