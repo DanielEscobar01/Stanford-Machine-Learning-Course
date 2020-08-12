eee58e776690a0dc30dc83c6e571b716a919ca8f
Daniel Escobar <dany.dim@hotmail.com>
2020-08-12 03:39:43 -0500

    Function to compute the cost function.

--- computeCost.m
+++ computeCost.m
@@ -3,5 +3,5 @@ function J = computeCost(X, y, theta) %This function compute the cost for linear
 	% X order is (97x2) so we have to transpose in order to multiply with theta
 	% X and y are matrices whose rows represent the examples from the training set
 	m = length(y); % M = Number of training examples
-	J = 1/(2*m) * sum(((X*theta)-y).^2);
+	J = 1/(2*m) * sum(((X*theta)-y).^2); 
 end
